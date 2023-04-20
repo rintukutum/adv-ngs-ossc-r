89aab79923a6c6f3debf8debf58eb2985a78bdcb
Rintu Kutum <rintukutum@gmail.com>
2023-04-20 19:40:26 +0530

    ngs 2

--- ngs-class-02.R
+++ ngs-class-02.R
@@ -2,4 +2,43 @@
 
 ### SMALL HEADER
 
+```{R}
+# vectors
+# character vector, numeric vector, logical vector & factor
 
+# single element
+char_vec = 'hello'
+
+# multiple elements
+char_vec_many = c('a','c','h')  
+
+# numeric vector
+num_vec <- c(1, 4.5, 8, 10)
+
+# character vec
+xg = c(2, "a", 10)
+
+# logical vector
+logical_vec <- char_vec_many == 'c'
+
+char_vec == "Hello"
+char_vec
+char_vec == "hello"
+1==1.1
+1==1.0
+1==1.00000000001
+char_vec_many == 'c'
+logical_vec <- char_vec_many == 'c'
+logical_vec
+logical_vec[1]
+logical_vec[2]
+char_vec_many[2]
+char_vec_many[logical_vec]
+!FALSE
+char_vec_many[!logical_vec]
+1 != 2
+
+
+
+
+```
